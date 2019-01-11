dfd7906a9aa4fb5601cb85998ef4df6e6c163d7a
Rommel Rico <me@rommelrico.com>
2019-01-10 21:34:37 -0800

    Alternate syntax
    Function that returns a function that returns a function

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,7 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (userId) => dispatch => {
-    _fetchUser(userId, dispatch);
-};
-
+export const fetchUser = (userId) => dispatch => _fetchUser(userId, dispatch);
 const _fetchUser = _.memoize(async (userId, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${userId}`);
     dispatch({ type: 'FETCH_USER', payload: response.data });
