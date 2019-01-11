d285ddcbed86b4e3f572e98739b84afe57640606
Rommel Rico <me@rommelrico.com>
2019-01-11 07:04:13 -0800

    Initial work on fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+// Solution to avoid multiple calls to fetchUser
+export const fetchPostAndUsers = () => async dispatch => {
+    const posts = await dispatch(fetchPosts());
+};
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
