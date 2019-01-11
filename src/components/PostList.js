ad617d712f23c62ac05cdf135052b7c565cd298c
Rommel Rico <me@rommelrico.com>
2019-01-11 07:05:06 -0800

    Unused import

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts, fetchPostAndUsers } from "../actions";
+import { fetchPostAndUsers } from "../actions";
 
 import UserHeader from './UserHeader';
 
