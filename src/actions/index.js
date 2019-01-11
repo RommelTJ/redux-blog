7e22619c11aefa2a82956b3cc6985ea5c2826608
Rommel Rico <me@rommelrico.com>
2019-01-10 21:29:54 -0800

    Reverting our original code

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,14 +6,7 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// export const fetchUser = (userId) => async dispatch => {
-//     const response = await jsonPlaceholder.get(`/users/${userId}`);
-//     dispatch({ type: 'FETCH_USER', payload: response.data });
-// };
-
-export const fetchUser = function(userId) {
-    return async function(dispatch) {
-        const response = await jsonPlaceholder.get(`/users/${userId}`);
-        dispatch({type: 'FETCH_USER', payload: response.data});
-    }
+export const fetchUser = (userId) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
 };
